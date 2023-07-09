c74f424940879a48963a0b7f77352d31f5b126fb
Renchester Ramos <renchesterjramos@gmail.com>
2023-07-09 17:03:19 +0800

    Add logo SVGs and tech stack dictionary

---
+++ src/config/stacks.ts
@@ -0,0 +1,104 @@
+const stacks = {
+  react: {
+    title: 'React',
+    logo: '/logos/react.svg',
+  },
+  next: {
+    title: 'Next.js',
+    logo: '/logos/next.svg',
+  },
+  sass: {
+    title: 'Sass',
+    logo: '/logos/sass.svg',
+  },
+  tailwind: {
+    title: 'TailwindCSS',
+    logo: '/logos/tailwind.svg',
+  },
+  javascript: {
+    title: 'Javascript',
+    logo: '/logos/javascript.svg',
+  },
+  typescript: {
+    title: 'Typescript',
+    logo: '/logos/typescript.svg',
+  },
+  node: {
+    title: 'Node.js',
+    logo: '/logos/node.svg',
+  },
+  express: {
+    title: 'Express.js',
+    logo: '/logos/express.svg',
+  },
+  mongodb: {
+    title: 'MongoDB',
+    logo: '/logos/mongodb.svg',
+  },
+  mongoose: {
+    title: 'Mongoose',
+    logo: '/logos/mongoose.svg',
+  },
+  react_router: {
+    title: 'React Router',
+    logo: '/logos/react-router.svg',
+  },
+  git: {
+    title: 'Git',
+    logo: '/logos/git.svg',
+  },
+  github: {
+    title: 'Github',
+    logo: '/logos/github.svg',
+  },
+  jest: {
+    title: 'Jest',
+    logo: '/logos/jest.svg',
+  },
+  vite: {
+    title: 'Vite',
+    logo: '/logos/vite.svg',
+  },
+  vitest: {
+    title: 'Vitest',
+    logo: '/logos/vitest.svg',
+  },
+  html: {
+    title: 'HTML5',
+    logo: '/logos/html.svg',
+  },
+  css: {
+    title: 'CSS3',
+    logo: '/logos/css.svg',
+  },
+  firebase: {
+    title: 'Firebase',
+    logo: '/logos/firebase.svg',
+  },
+  linux: {
+    title: 'Linux',
+    logo: '/logos/linux.svg',
+  },
+  notion: {
+    title: 'Notion',
+    logo: '/logos/notion.svg',
+  },
+  webpack: {
+    title: 'Webpack',
+    logo: '/logos/webpack.svg',
+  },
+  affinity_photo: {
+    title: 'Affinity Photo',
+    logo: '/logos/affinity-photo.svg',
+  },
+  affinity_publisher: {
+    title: 'Affinity Publisher',
+    logo: '/logos/affinity-publisher.svg',
+  },
+  affinity_designer: {
+    title: 'Affinity Designer',
+    logo: '/logos/affinity-designer.svg',
+  },
+};
+
+export default stacks;

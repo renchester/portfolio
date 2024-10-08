4cc12cc5b0fddbe06576cca92db4a32bde85d047
Renchester Ramos <renchesterjramos@gmail.com>
2024-10-08 22:54:57 +0800

    Install sanity and add related files

---
+++ src/sanity/structure.ts
@@ -0,0 +1,17 @@
+import type { StructureResolver } from 'sanity/structure';
+
+// https://www.sanity.io/docs/structure-builder-cheat-sheet
+export const structure: StructureResolver = (S) =>
+  S.list()
+    .title('Portfolio')
+    .items([
+      S.documentTypeListItem('author').title('Author Details'),
+      S.divider(),
+      S.documentTypeListItem('greeting').title('Greetings'),
+      S.documentTypeListItem('project').title('Projects'),
+      S.documentTypeListItem('stack').title('Tech Stack'),
+      S.divider(),
+      S.documentTypeListItem('post').title('Posts'),
+      S.documentTypeListItem('category').title('Categories'),
+      S.divider(),
+    ]);
